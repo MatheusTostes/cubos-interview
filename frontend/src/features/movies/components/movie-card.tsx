65f90c8ab9ce6f316c80202bf3be3d4dda57cf7c
Matheus Tostes <matheus_tostes@hotmail.com>
2025-10-26 02:25:57 -0300

    feat(movies): change movie types and mock for imdb standard

--- frontend/src/features/movies/components/movie-card.tsx
+++ frontend/src/features/movies/components/movie-card.tsx
@@ -10,18 +10,18 @@ export const MovieCard = ({ movie }: { movie: Movie }) => {
     >
       <Card.Content className="h-full w-full p-0">
         <img
-          src={movie.poster}
-          alt={movie.title}
+          src={movie.primaryImage?.url ?? ''}
+          alt={movie.primaryTitle}
           className="h-full w-full object-cover"
         />
       </Card.Content>
 
       <Card.Footer className="absolute bottom-0 left-0 right-0 flex flex-col items-start gap-2 bg-gradient-to-t from-white to-transparent  p-4 pt-20 dark:from-black dark:to-transparent">
         <Typography variant="h5" className="font-semibold" font="montserrat">
-          {movie.title}
+          {movie.primaryTitle}
         </Typography>
         <Typography variant="span" className="text-[12.8px]" font="montserrat">
-          {movie.categories.join(', ')}
+          {movie.genres.join(', ')}
         </Typography>
       </Card.Footer>
     </Card.Root>
--- frontend/src/features/movies/types/movie-types.tsx
+++ frontend/src/features/movies/types/movie-types.tsx
@@ -1,8 +1,26 @@
 export type Movie = {
-  id: number
-  title: string
-  poster: string
-  categories: string[]
-  rating: number
-  description: string
+  id: string
+  type: string
+  primaryTitle: string
+  originalTitle: string
+  primaryImage: {
+    url: string
+    width: number
+    height: number
+  }
+  startYear: number
+  endYear?: number
+  runtimeSeconds?: number
+  genres: string[]
+  rating: {
+    aggregateRating: number
+    voteCount: number
+  }
+  plot: string
+}
+
+export type MoviesResponse = {
+  titles: Movie[]
+  totalCount: number
+  nextPageToken: string
 }
